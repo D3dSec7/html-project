b25fdf769d832a459940eba116770cf5356989ae
D3dSec7 <nottronin@gmail.com>
2024-12-13 08:06:31 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 function aston() {
     window.location.href = 'Aston Martin Gallery.html';
 }
+
 function ferrari() {
     window.location.href = 'Ferrari Gallery.html';
 }
+
 function mclaren() {
     window.location.href = 'Mclaren Gallery.html';
 }
+
 function mercedes() {
     window.location.href = 'Mercedes Gallery.html';
 }
+
 function porsche() {
     window.location.href = 'Porsche Gallery.html';
 }
+
 function book() {
     alert("car is now booked");
 }
+
+function submit() {
+    alert("message sent");
+}  
